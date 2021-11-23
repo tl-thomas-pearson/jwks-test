0d170ece7b49eac80577ea064f55f58c1359444e
tl-thomas-pearson <thomas.pearson@truelayer.com>
2021-11-23 18:50:09 +1300

    Send json back on redirect.

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ server.use(express.json());
 server.get("/keys", (req, res) => res.json(jwks_data));
 
 server.get("/redirect-page*", (req, res) => {
-    console.log("`/redirect-page` received:", { body: req.body, query: req.query });
-    res.sendStatus(200);
+    const json = { body: req.body, query: req.query };
+    console.log("`/redirect-page` received:", json);
+    res.json(json);
 })
 
 
