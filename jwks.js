9d76224baf6fc05ac9eca59f5a770b9fa74f8deb
tl-thomas-pearson <thomas.pearson@truelayer.com>
2021-12-02 11:02:20 +1300

    clean up.

--- jwks.js
+++ jwks.js
@@ -2,34 +2,12 @@ const jwks = {
   keys: [
     {
       kty: "RSA",
-      kid: "uBRiGnOhJhNnopxdhxspKbCRfNjzKe4YGBDHgynbCTU",
-      use: "sig",
-      alg: "PS256",
-      e: "AQAB",
-      n: "vdmYgOx80TmJ9uCD8XU9CYTNlObSFuqTmmDgVDq557gYTO_ep82rM5Mex5rSzzgkOfzbQrTOGXn9xqaQD2Hr1M_pDmRsR4OOah4ffIusP3IIxczVeGniyHtB6yzDJs9lB7ZCqkQtvGF7vs4CJpSjDL3_VJPsabSRKbDJiGY6uN-ULC6Bb2OHAtgOQzjAErO7vEgD_JVIjGtkmgCfoiFTQGUswE8dvFaMFspsdMFyaEBF-yLoIWad-BZ8Shv-aiUSuOa0HKQa_JN4hfOYwrD1AKWNkHhCYdywA1zOOEoICiDVMkfgVEKygEy_N_T4SPcwhIJ2yN6oWTDjmHmNyULjOQ",
-      d: "D2EaLo95yQUtPPeRGLVTnXrJoJ9WM48LJ_1jDs-3EaNjLJBd7CAIF2R6oNbU7v5wSNZH-M5mx7-PJFGH6AcZsKKrEYRnFR_-IHCSQmeaK4WUWlmujVOekHkTrPZyVKmGWKqWGe7t-Y24FYvgtrlhv_2AOpzFEQcUn1j36nnFlgY60WN3krXniYiEOrMY8xqilrkE1qpcXvvOemsAsr5G_i135pOSsfRhkf8cf_6TG0gCAymgIHmwS4JVeCuTyZZmBY2hLUEfGJ5MQAjuO2atkXJUHvwExQg3GivVE0_KATyjEkaHcaf9K2wdzuqB-X9YTPbUbckQPUfhXcd86OodAQ",
-      p: "668AQgEAwGTnnQJsPhvf2MMq_15MTLVheivmFOwKAob2pEHb0XqrJhYAZqED3RO8vU9NA1FVs5jJ3dsQ9CnMKAqfQWQ9a1eYWdkpvo5iehwPesOgLttJIAz6kK-w8Sxm_VlqQ2rhdVI_43D8WSJNMy5prPUBNb4KKmmiG4UzR9k",
-      q: "zjcmxoj68jwlfu_rDD_aUI9Wa8avEFEfi1RUvN9SAwUdbGEocoSMB7b6zCagjM1Y_Vr88CO8lv2G6vuaKLfsiXi5Nk5mL9-9-7MMYhKCOLqjyey18sgoBnf0PRe4tNVmsm7AscgvAIwpcRzupTn8HjUbqaKb0a3nL7liesgJumE",
-      dp: "bMNh9xPHaQIn8MlBcpgigUHwAdFenLfHuD2x_E3BgNEP2VWbP9zARt84aCQ-SDPnkeWawME0L6F-5ydFd6giUwgz3lWIEpVl9XDGVu4WfAufCSRmM7l20ZerukGaAnJfROOSqnIUgbPnT-rASdIOlmQA2JRoboi44EHHUbPyqmE",
-      dq: "KXZLVJXMJxJS06_3iZH5GkOHs8nR_yzgc8oz83nPao4HuXfbgxMFeLOgv5VwqH7NSruVqc71p0I4c1y9RTyd0ZI1YybmQu_wqf_5GGryRee48OuoNbVq3soVkIcJe0qBCJVeJyDB2pFT78tKbGw5NfWlpzjrrYASwLS8iUVZN2E",
-      qi: "PV0RiSWedZb1is9yH-2di6hiDtLL7BlnNyw5n74EcgvFDsWXjwcbjzqiVmvOye-bc2P7u1hYIn6-eY7cplevtpmbnkyXlF6dk2C1YMLdHE0VN7pg48iV6ea5fUIzvIlq5pQe0WeDbE2aJt4WQqSI7TLjyjbE6nP9Tni2V9SooIw",
-    },
-    {
-      kty: "RSA",
       kid: "gBdaS-G8RLax2qgObTD94w",
       use: "sig",
       alg: "PS256",
       n: "o6nov6WjR9E-wcVKCSxjfKE4qaPaRBmblLnRNAhfwMUPNyCHEMdeKLmn-hyF6xxDt0G1A8g2gJnGs7dRc7SpaJbQRtq9dnkXpscshxMs1qrRLO0pfPkpjG4VpV8UpQqrGDkzy_8Bw7XKlJVCp1CE-BlTVZ8qyRLngMYWvsMSaLA-lGkB65X8s_CZrOoXJQVOlVfeWQTWgszAQUWBMlZrVYR-mekZE-BNniysvQXqwfsO-4lxC0RxLZ_QSIHc3bvkaFmA4HowcbrS5twgxpi4it9lmCNGjyPoPJ3PSTI8aBzmTYVjnRabVHujig2K4_TiLnz7canjTO-X0UEPKfwA-w",
-      e: "AQAB",
+      e: "AQAB"
     },
-    // {
-    //   "kty": "RSA",
-    //   "e": "AQAB",
-    //   "use": "enc",
-    //   "kid": "PstJP83mC49x2q8dWBSqXp-ECAVFcSBUaBhW9IhlPsE",
-    //   "alg": "RSA-OAEP",
-    //   "n": "o6nov6WjR9E-wcVKCSxjfKE4qaPaRBmblLnRNAhfwMUPNyCHEMdeKLmn-hyF6xxDt0G1A8g2gJnGs7dRc7SpaJbQRtq9dnkXpscshxMs1qrRLO0pfPkpjG4VpV8UpQqrGDkzy_8Bw7XKlJVCp1CE-BlTVZ8qyRLngMYWvsMSaLA-lGkB65X8s_CZrOoXJQVOlVfeWQTWgszAQUWBMlZrVYR-mekZE-BNniysvQXqwfsO-4lxC0RxLZ_QSIHc3bvkaFmA4HowcbrS5twgxpi4it9lmCNGjyPoPJ3PSTI8aBzmTYVjnRabVHujig2K4_TiLnz7canjTO-X0UEPKfwA-w"
-    // },
     {
       "kty": "RSA",
       "e": "AQAB",
